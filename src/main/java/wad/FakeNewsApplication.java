9c67836cc4f25aabeaa078420a904cabf054525a
responderi <rv.mahti@gmail.com>
2017-11-28 01:55:11 +0200

    alustuksen viimeistely

--- src/main/java/wad/FakeNewsApplication.java
+++ src/main/java/wad/FakeNewsApplication.java
@@ -2,11 +2,21 @@ package wad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class FakeNewsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FakeNewsApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeNewsApplication.class, args);
+    }
 }
