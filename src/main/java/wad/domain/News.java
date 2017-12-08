abc57e1b73208193fb6d5df066444be7c629a0ef
responderi <rv.mahti@gmail.com>
2017-12-09 01:25:52 +0200

    update

--- src/main/java/wad/domain/News.java
+++ src/main/java/wad/domain/News.java
@@ -2,20 +2,19 @@
 package wad.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
  *
  * @author rov
  */
-@NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
@@ -24,11 +23,15 @@ public class News extends AbstractPersistable<Long> {
     private String title;
     private String lead;
     private String text;
-    private LocalDate releaseDate;
+    private LocalDateTime releaseDate;
     @ManyToMany
     private List<Author> authors;
     @ManyToMany
     private List<Category> categories;
+    
+    public News(){
+        this.releaseDate = LocalDateTime.now();
+    }
     
     public void addAuthor(Author author) {
         if (this.authors == null) {
